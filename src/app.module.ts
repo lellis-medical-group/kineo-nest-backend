8e1e1a19364e2dcee3c2c2a0911a32db67697469
Epitexam <202238868+epitexam@users.noreply.github.com>
2026-08-09 16:30:37 +0200

    chore(profile): scaffold module via nest cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { AuthModule } from "@thallesp/nestjs-better-auth";
 import { auth } from './lib/auth'
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from "@nestjs/config";
+import { ProfileModule } from './profile/profile.module';
 import configuration from "./config/configuration";
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [configuration] }), AuthModule.forRoot({ auth }), UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [configuration] }), AuthModule.forRoot({ auth }), UsersModule, ProfileModule],
   controllers: [
     AppController,
   ],
